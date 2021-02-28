1ec878988f936b43a697a3c07bfadf9707439660
rebecca <rebecca.rothschild@wsu.edu>
2021-02-28 00:39:24 -0800

    Added the files and refined the thumbnail grid

--- client/app/components/thumbnail.js
+++ client/app/components/thumbnail.js
@@ -3,6 +3,5 @@ import Component from '@glimmer/component';
 export default class ThumbnailComponent extends Component {
     constructor() {
         super(...arguments);
-        this.filePath = `path: ${this.args.fileName}`;
     }
 }
