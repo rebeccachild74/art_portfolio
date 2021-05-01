b60c7f46a9af95a0907eff7723dda544380d825b
rebecca <rebecca.rothschild@wsu.edu>
2021-04-30 20:13:06 -0700

    Removed art-grid passthrough component

--- client/app/routes/artwork.js
+++ client/app/routes/artwork.js
@@ -1,4 +1,49 @@
 import Route from '@ember/routing/route';
 
+class Artwork {
+    constructor({ pieces }) {
+        this.pieces = pieces;
+    }
+}
+class Piece {
+    constructor({ id, name, medium, filePath }) {
+        this.id = id;
+        this.name = name;
+        this.filePath = filePath;
+        this.medium = medium;
+    }
+}
+
 export default class ArtworkRoute extends Route {
+    model() {
+        let cat = new Piece({
+            id: 'cat',
+            name: 'Funny Cat',
+            filePath: "cat1.jpg",
+            medium: 'Photograph, 35mm',
+        });
+
+        let fish = new Piece({
+            id: 'fish',
+            name: 'Puffer Fish',
+            filePath: "puffer.jpg",
+            medium: 'Photograph, 35mm',
+        });
+
+        let tree = new Piece({
+            id: 'tree',
+            name: 'Tall Tree',
+            filePath: "tree.jpg",
+            medium: 'Oil on canvas',
+        });
+
+        let dog = new Piece({
+            id: 'dog',
+            name: 'Wiener Dog',
+            filePath: "weinerdog.jpg",
+            medium: 'Digital Image',
+        });
+
+        return [cat, fish, tree, dog];
+    }
 }
--- client/app/routes/artwork/piece.js
+++ client/app/routes/artwork/piece.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default class ArtworkPieceRoute extends Route {
+    model(params) {
+        let pieces = this.modelFor('artwork');
+        return pieces.find((piece) => piece.id === params.id);
+    }
 }
