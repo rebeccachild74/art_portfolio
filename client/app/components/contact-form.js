dc8cc50b9e8c3bce9668d0a344c8df7f3d4933e3
rebecca <rebecca.rothschild@wsu.edu>
2021-05-12 15:30:11 -0700

    Updated about and added buy icon to info pages

--- client/app/components/contact-form.js
+++ client/app/components/contact-form.js
@@ -6,6 +6,7 @@ export default class ContactFormComponent extends Component {
     }
 
     get availablePrintSizes() {
+        // Add logic based on the piece dimensions
         return this.args.availablePrintSizes ?? ['16 x 20', '12 x 12'];
     }
 
