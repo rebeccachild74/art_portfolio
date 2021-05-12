37667943453244ceb6980a5e1fb65fc3ccc1fc98
rebecca <rebecca.rothschild@wsu.edu>
2021-05-11 21:18:21 -0700

    Added a contact-form component and testing it in about template

---
+++ client/app/components/contact-form.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+
+export default class ContactFormComponent extends Component {
+    get pieceName() {
+        return this.args.pieceName ?? 'Test123';
+    }
+
+    get availablePrintSizes() {
+        return this.args.availablePrintSizes ?? ['16 x 20', '12 x 12'];
+    }
+
+    get isSellingPrints() {
+        return this.args.isSellingPrints ?? false;
+    }
+}
--- client/app/services/piece-details.js
+++ client/app/services/piece-details.js
@@ -1,11 +1,13 @@
 import Service from '@ember/service';
 
 class Piece {
-    constructor({ id, name, medium, filePath }) {
+    constructor({ id, name, medium, filePath, sellingPrints, dimensions }) {
         this.id = id;
         this.name = name;
         this.filePath = filePath;
         this.medium = medium;
+        this.sellingPrints = sellingPrints;
+        this.dimensions = dimensions;
     }
 }
 
@@ -20,24 +22,32 @@ export default class PieceDetailsService extends Service {
                 name: 'Funny Cat',
                 filePath: "cat1.jpg",
                 medium: 'Photograph, 35mm',
+                sellingPrints: true,
+                dimensions: '8 x 11',
             }),
             new Piece({
                 id: 'fish',
                 name: 'Puffer Fish',
                 filePath: "puffer.jpg",
                 medium: 'Photograph, 35mm',
+                sellingPrints: true,
+                dimensions: '8 x 11',
             }),
             new Piece({
                 id: 'tree',
                 name: 'Tall Tree',
                 filePath: "tree.jpg",
                 medium: 'Oil on canvas',
+                sellingPrints: true,
+                dimensions: '12 x 12',
             }),
             new Piece({
                 id: 'dog',
                 name: 'Wiener Dog',
                 filePath: "weinerdog.jpg",
                 medium: 'Digital Image',
+                sellingPrints: true,
+                dimensions: '16 x 20',
             })
         ];
     }
