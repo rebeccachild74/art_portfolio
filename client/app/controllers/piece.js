82caac20b1621800852a2ee4115ff62bdfa5946f
rebecca <rebecca.rothschild@wsu.edu>
2021-06-06 22:25:46 -0700

    Added basic contact form elements

--- client/app/controllers/piece.js
+++ client/app/controllers/piece.js
@@ -3,12 +3,11 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class PieceController extends Controller {
-    @tracked showContactForm = true; // should default to false
+    @tracked showContactForm = false; // should default to false
 
     @action
     updateFormVisibility() {
         this.showContactForm = !this.showContactForm;
-        console.log(`Clicked!! Now value is: ${this.showContactForm}`);
     }
 
     setFormVisibilityFalse() {
